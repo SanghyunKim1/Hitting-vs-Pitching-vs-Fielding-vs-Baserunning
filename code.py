676e6bdd6b6934ac7a3216969f32b010cac1fd97
Sanghyun Kim <sanghyunkim@Sanghyuns-MacBook-Pro.local>
2021-02-02 18:56:41 +0900

    Code edit
    Selected features

--- code.py
+++ code.py
@@ -1,14 +1,16 @@
-from pybaseball import statcast
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import StandardScaler
 from sklearn import linear_model, metrics
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from math import sqrt
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+import statsmodels.api as sm
 import warnings
 warnings.filterwarnings('ignore')
 from matplotlib.axes._axes import _log as matplotlib_axes_logger
@@ -19,7 +21,6 @@ matplotlib_axes_logger.setLevel('ERROR')
 # load datasets
 batting_df = pd.read_csv('/Users/sanghyunkim/Desktop/Data Science Project/MLB Analysis/Offense-Pitching-Fielding-and-Baserunning/data/FanGraphs Team Batting Data.csv')
 pitching_df = pd.read_csv('/Users/sanghyunkim/Desktop/Data Science Project/MLB Analysis/Offense-Pitching-Fielding-and-Baserunning/data/FanGraphs Team Pitching Data.csv')
-
 
 
 ### 1. Data Cleaning ###
@@ -36,55 +37,119 @@ pitching_df.rename(columns={'BABIP': 'oppBABIP'}, inplace=True)
 pitching_df.rename(columns={'EV': 'oppEV'}, inplace=True)
 pitching_df.rename(columns={'WAR': 'pWAR'}, inplace=True)
 
+# merge datasets
+team_df = pd.merge(batting_df, pitching_df, on=['Season', 'Team'], how='outer')
+
+# create new columns
+team_df['UER'] = team_df['RA'] - team_df['ER']
+team_df['G'] = team_df['W'] + team_df['L']
+team_df['wPCT'] = round((team_df['W']/team_df['G']), 3)
+
+# check data types
+print(team_df.dtypes)
+
+# categorical variables
+obj_cols = list(team_df.select_dtypes(include='object'))
+print(team_df[obj_cols].head())
+
 # number of missing values
-dfs = [batting_df, pitching_df]
-for df in dfs:
-    print('------- Missing Values -------')
-    print(df.isnull().sum())
+print('------- Missing Values -------')
+print(team_df.isnull().sum())
 
 # replace missing values with predicted values using a linear regression model
-no_obj = batting_df.select_dtypes(exclude='object')
+no_obj = team_df.select_dtypes(exclude='object')
 imputer = IterativeImputer(random_state=1).fit_transform(no_obj)
-batting_df = pd.DataFrame(data=imputer, columns=no_obj.columns)
-print(batting_df.isnull().sum())
+impute_df = pd.DataFrame(data=imputer, columns=no_obj.columns)
 
-no_obj = pitching_df.select_dtypes(exclude='object')
-imputer = IterativeImputer(random_state=1).fit_transform(no_obj)
-pitching_df = pd.DataFrame(data=imputer, columns=no_obj.columns)
-print(pitching_df.isnull().sum())
+team_df = pd.concat([team_df['Team'], impute_df], axis=1)
+
+print('------- Missing Values -------')
+print(team_df.isnull().sum())
 
 # number of duplicates
-print('Number of Duplicates in Batting Data: {}'.format(batting_df.duplicated().sum()))
-print('Number of Duplicates in Pitching Data: {}'.format(pitching_df.duplicated().sum()))
+print('Number of Duplicates: {}'.format(team_df.duplicated().sum()))
+
+# note: based on domain knowledge, I filtered the best features for this analysis
+# for more information, please refer to README.md on GitHub
+# drop unnecessary variables
+vars_keep = ['Season', 'Team', 'wPCT', 'wRC+', 'wOBA', 'WHIP', 'xFIP', 'Def', 'BsR']
+team_df = team_df[vars_keep]
+
+
 
 ### 2. EDA ###
-## 2-1, Batting Data EDA ##
 # data structure
-print('Data Structure: {}'.format(batting_df.shape))
+print('Data Structure: {}'.format(team_df.shape))
 
-# data types
-print("------- Data Types -------")
-print(batting_df.dtypes)
+# change data types
+team_df['Season'] = team_df['Season'].astype('int')
 
 # descriptive summaries
-print(batting_df.describe().to_string())
+print(team_df.describe().to_string())
 
 # correlation matrix
-corrMatrix = round(batting_df.corr(), 2)
+corrMatrix = team_df.corr()
+print(corrMatrix.to_string())
+
 mask = np.triu(np.ones_like(corrMatrix, dtype=bool))
+
 fig, ax = plt.subplots(figsize=(10, 10))
 
-sns.heatmap(corrMatrix, mask=mask, square=True, annot=True, annot_kws={'size': 8}, linewidths=.5,
+sns.heatmap(corrMatrix, mask=mask, annot=True, annot_kws={'size':10}, square=True, linewidths=.5,
             cbar_kws={'shrink': .5}, xticklabels=corrMatrix.columns, yticklabels=corrMatrix.columns, ax=ax)
 ax.set_title('Correlation Matrix')
 
 plt.show()
 
-seasonal_Bdf = batting_df.groupby('Season')
-seasonal_Bdf = seasonal_Bdf['wRC+'].mean().reset_index()
 
-fig, ax = plt.subplots(figsize=(10, 10))
-plt.plot(seasonal_Bdf['Season'], seasonal_Bdf['wRC+'], '-r')
-ax.set_title('Yearly Changes in Team wRC+')
-ax.set_xticks(range(1870, 2021, 10))
-plt.show()
+
+
+
+# # feature scaling
+# data = team_df[['wPCT', 'wRC+', 'wOBA', 'WHIP', 'xFIP', 'Def', 'BsR']]
+# scale = data.iloc[:, data.columns != 'wPCT']
+# cols = list(scale.columns)
+#
+# scaler = StandardScaler()
+# scaled_data = scaler.fit_transform(scale)
+# scaled_df = pd.DataFrame(scaled_data, columns=cols)
+#
+# data = pd.concat([data['wPCT'], scaled_df], axis=1)
+#
+# x = data.iloc[:, data.columns != 'wPCT']
+# y = data['wPCT']
+#
+# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
+#
+# lm = linear_model.LinearRegression().fit(x_train, y_train)
+# y_predict = lm.predict(x_test)
+#
+# print(lm.intercept_)
+# print(lm.coef_)
+# print(sqrt(metrics.mean_squared_error(y_test, y_predict)))
+# print(metrics.r2_score(y_test, y_predict))
+#
+# x = data.iloc[:, data.columns != 'wPCT']
+# x = sm.add_constant(x)
+# y = data['wPCT']
+#
+# lm = sm.OLS(y, x)
+# result = lm.fit()
+#
+# print(result.summary())
+#
+# vif = pd.DataFrame()
+# vif['Feature'] = lm.exog_names
+# vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
+# print(vif[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
+#
+# x = data.iloc[:, data.columns != 'wPCT']
+# y = data['wPCT']
+#
+# model = LinearRegression()
+# cv_r2 = cross_val_score(model, x, y, scoring='r2', cv=10)
+# cv_mse = cross_val_score(model, x, y, scoring='neg_mean_squared_error', cv=10)
+# cv_rmse = np.sqrt(-1 * cv_mse)
+#
+# print(cv_r2.mean())
+# print(cv_rmse.mean())
